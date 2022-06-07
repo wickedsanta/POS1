3042718321fb7f2ce63afc8412098c774341f370
demizone420 <106924527+demizone420@users.noreply.github.com>
2022-06-07 12:04:20 +0200

    Update SURFMETEOc.cpp

--- src/main/SURFMETEOc.cpp
+++ src/main/SURFMETEOc.cpp
@@ -4,7 +4,6 @@
 #include <math.h>
 #include "BME680.h"
 
-#define Bucket_Size 0.01 // bucket size to trigger tip count
 #define RG11_Pin 3 // digital pin RG11 connected to
 #define TX_Pin 8 // used to indicate web data tx
 #define WaterSensor_Pin (A5) // analog pin for water temperature sensor
@@ -50,8 +49,55 @@ rotations = 0;
 isSampleRequired = false;
 
 // setup water temp sensor
+  byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+  byte addr[8];
+  float celsiusWATER;
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
   
+  delay(1000);     // maybe 750ms is enough, maybe not
+  // we might do a ds.depower() here, but the reset will take care of it.
   
+  present = ds.reset();
+  ds.select(addr);    
+  ds.write(0xBE);         // Read Scratchpad
+ 
+  Serial.print("  Data = ");
+  Serial.print(present, HEX);
+  Serial.print(" ");
+  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+    data[i] = ds.read();
+    Serial.print(data[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.print(" CRC=");
+  Serial.print(OneWire::crc8(data, 8), HEX);
+  Serial.println();
+ 
+  // Convert the data to actual temperature
+  // because the result is a 16 bit signed integer, it should
+  // be stored to an "int16_t" type, which is always 16 bits
+  // even when compiled on a 32 bit processor.
+  int16_t raw = (data[1] << 8) | data[0];
+  if (type_s) {
+    raw = raw << 3; // 9 bit resolution default
+    if (data[7] == 0x10) {
+      // "count remain" gives full 12 bit resolution
+      raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  } else {
+    byte cfg = (data[4] & 0x60);
+    // at lower res, the low bits are undefined, so let's zero them
+    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+    //// default is 12 bit resolution, 750 ms conversion time
+  }
+  celsiusWATER = (float)raw / 16.0;  
 // setup timer values
 timerCount = 0;
   
@@ -108,6 +154,9 @@ client.println("<html><body>");
 digitalWrite(TX_Pin,HIGH); // Turn the TX LED on
 client.print("<span style=\"font-size: 26px\";><br>  Temperatura: ");
 client.print(ds.getTemperature_C());
+client.println(" °C<br>");
+client.print("<span style=\"font-size: 26px\";><br>  Temperatura wody: ");
+client.print(ds.getTemperature_C_w());
 client.println(" °C<br>");
 client.print("%<br>  Cisnienie: ");
 client.print(bme.getPressure_MB());
